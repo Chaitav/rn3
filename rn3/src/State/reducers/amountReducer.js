1a3e687805cbf47deb5ded7bf7871b5d653ca88f
Chaitav <81869864+Chaitav@users.noreply.github.com>
2022-06-08 08:30:32 +0530

    Add files via upload

---
+++ rn3/src/State/reducers/amountReducer.js
@@ -0,0 +1,12 @@
+const reducer = (action, state=0) => {
+    if (action.type === 'deposit') {
+        return state + action.payload
+    }
+    else if (action.type === 'withdraw') {
+        return state - action.payload
+    }
+    else {
+        return state;
+    }
+}
+export default reducer;
